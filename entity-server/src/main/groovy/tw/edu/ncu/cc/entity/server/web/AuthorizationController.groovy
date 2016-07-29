7fcb8c9a1d34a4d627d2ca8e75a33624a231d347
JasonChiu <jason1122g@gmail.com>
2016-07-29 16:31:09 +0800

    Add authorization index api

--- entity-server/src/main/groovy/tw/edu/ncu/cc/entity/server/web/AuthorizationController.groovy
+++ entity-server/src/main/groovy/tw/edu/ncu/cc/entity/server/web/AuthorizationController.groovy
@@ -2,6 +2,8 @@ package tw.edu.ncu.cc.entity.server.web
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.convert.ConversionService
+import org.springframework.core.convert.TypeDescriptor
+import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication
@@ -11,6 +13,7 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.bind.annotation.*
 import tw.edu.ncu.cc.entity.data.v1.AuthorizationObject
+import tw.edu.ncu.cc.entity.server.model.EntityAuthorization
 import tw.edu.ncu.cc.entity.server.operation.AuthorizationOperation
 import tw.edu.ncu.cc.entity.server.validator.AuthorizationCreateValidator
 
@@ -27,6 +30,26 @@ public class AuthorizationController extends BaseController {
     @InitBinder
     public static void initBinder(WebDataBinder binder ) {
         binder.addValidators( new AuthorizationCreateValidator() )
+    }
+
+    @PreAuthorize( value = "hasRole('admin')" )
+    @RequestMapping( method = RequestMethod.GET )
+    def index( @RequestParam( value = "authorizee_id", required = true ) final String authorizeeId,
+               @RequestParam( value = "entity_id", required = true ) final String entityId,
+               Pageable pageable, Authentication authentication ) {
+
+        logger.info( "authorization index, authorizee:{}, entity:{}, size:{}, number:{}, offset:{}, operator:{}",
+                authorizeeId, entityId, pageable.pageSize, pageable.pageNumber, pageable.offset, authentication.name)
+
+        def entityPages = authorizationOperation.index( pageable, new AuthorizationObject( authorizeeId: authorizeeId, entityId: entityId) )
+
+        def entityObjects = conversionService.convert(
+                entityPages.content,
+                TypeDescriptor.collection( List.class, TypeDescriptor.valueOf( EntityAuthorization.class ) ),
+                TypeDescriptor.array( TypeDescriptor.valueOf( AuthorizationObject.class ) )
+        )
+
+        return toPageObjects( entityObjects, entityPages )
     }
 
     @PreAuthorize( value = "hasRole('admin')" )
